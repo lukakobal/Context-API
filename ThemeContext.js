6e282b4aaabcf5a29302e3fc265dbdefb316caf9
Luka Kobal <info.radialart@gmail.com>
2026-01-11 10:43:54 +0100

    Create ThemeContext.js

---
+++ ThemeContext.js
@@ -0,0 +1,11 @@
+import { createContext, useState } from "react";
+
+export const ThemeContext = createContext();
+
+export function ThemeProvider({ children }) {
+  const [theme] = useState("light");
+
+  return (
+    <ThemeContext.Provider value={theme}>{children}</ThemeContext.Provider>
+  );
+}
